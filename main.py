56b522c652832b05900720556de7eb463c41c624
shay-dr <shay.davidr@gmail.com>
2025-07-26 17:18:10 -0600

    Cleaned up verbose code and usr_prompt.
    Note: I purposely did not use argparse as I was already using sys.argv and want to keep imported libraries to a minimum.  In the case that I expand this, I should revisit that decision.

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-verbose_mode = False
-
-# Verbose
-if '--verbose' in sys.argv:
-    verbose_mode = True
-    sys.argv.remove('--verbose') 
-
 
 def main():
     #Check for command line argument
     if len(sys.argv) < 2:
-        print("Error: Please add a quoted usr_prompt as an argument")
+        print("Error: Please add a quoted user_prompt as an argument")
         sys.exit(1)
 
-    usr_prompt = sys.argv[1]
+    verbose_mode = False
+
+    # Verbose
+    if '--verbose' in sys.argv:
+        verbose_mode = True
+        sys.argv.remove('--verbose')
     
+
+    # Assign user_prompt to argument
+    user_prompt = sys.argv[1]
+
     #check for empty/whitespace
-    if not usr_prompt.strip():
+    if not user_prompt.strip():
         print("Error: The quoted prompt cannot be empty")
         sys.exit(1)
 
@@ -35,7 +37,7 @@ def main():
 
     #create a messages list to be used to maintain context later.
     messages = [
-    types.Content(role="user", parts=[types.Part(text=usr_prompt)]),
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
     # Create Gemini client and get reply:
@@ -50,11 +52,9 @@ def main():
     response_tokens = response.usage_metadata.candidates_token_count
 
     # Display results with or without verbose
-    if verbose_mode == True:
-        print(f"User prompt:  {usr_prompt} \n Prompt tokens: {prompt_tokens} \n Response tokens: {response_tokens}\n")
-    print(f"{response.text}")
-    
-        
+    if verbose_mode:
+        print(f"User prompt:  {user_prompt} \n Prompt tokens: {prompt_tokens} \n Response tokens: {response_tokens}\n")
+    print(f"{response.text}")    
 
 
 if __name__ == "__main__":
